80c2df6a973bf1b184035267132bf5a387318879
Jon Morrow <jon@morrowmail.com>
2024-10-16 14:18:12 -0700

    Working with Enums
    Signed-off-by: Jon Morrow <jon@morrowmail.com>

--- Section 2: TypeScript Basics & Basic Types/app.ts
+++ Section 2: TypeScript Basics & Basic Types/app.ts
@@ -2,16 +2,29 @@
 //     name: string; 
 //     age: number;
 // } = {
-const person: {
-    name: string;
-    age: number;
-    hobbies: string[];
-    role: [number, string];
-} = {
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string];
+// } = {
+//     name: "Jon",
+//     age: 30,
+//     hobbies: ['Sports', 'Cooking'],
+//     role: [2, 'author']
+// };
+
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role { ADMIN = 'ADMIN', READ_ONLY = 100, AUTHOR = 'AUTHOR' };
+
+const person = {
     name: "Jon",
     age: 30,
     hobbies: ['Sports', 'Cooking'],
-    role: [2, 'author']
+    role: Role.ADMIN
 };
 
 // person.role.push('admin');
@@ -28,4 +41,8 @@ console.log(person.name);
 for (const hobby of person.hobbies) {
     console.log(hobby.toUpperCase());
     // console.log(hobby.map());  // !!! ERROR !!!xs
+}
+
+if (person.role === Role.AUTHOR) {
+    console.log('is author');
 }
