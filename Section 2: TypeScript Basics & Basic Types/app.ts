cc711229e5a8b73af07cdaf5a0735f778aceca19
Jon Morrow <jon@morrowmail.com>
2024-10-16 11:27:32 -0700

    Type Assignment & Type Inference
    Signed-off-by: Jon Morrow <jon@morrowmail.com>

--- Section 2: TypeScript Basics & Basic Types/app.ts
+++ Section 2: TypeScript Basics & Basic Types/app.ts
@@ -10,7 +10,8 @@ function add(n1: number, n2: number, showResult: boolean, phrase: string) {
     }
 }
 
-const number1 = 5; // 5.0
+let number1: number
+number1 = 5; // 5.0
 const number2 = 2.8;
 const printResult = true;
 const resultPhrase = "Result is: ";
