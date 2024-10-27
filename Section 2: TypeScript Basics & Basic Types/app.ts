dd633201775be37b816f8106bc0652dffc96c24d
Jon Morrow <jon@morrowmail.com>
2024-10-27 13:37:43 -0700

    The "never" type
    Signed-off-by: Jon Morrow <jon@morrowmail.com>

--- Section 2: TypeScript Basics & Basic Types/app.ts
+++ Section 2: TypeScript Basics & Basic Types/app.ts
@@ -7,3 +7,10 @@ userInput = 'Jon';
 if (typeof userInput === 'string') {
     userName = userInput;
 }
+
+function generateError(message: string, code: number): never {
+    throw {message: message, errorCode: code };
+    // while (true) {}
+}
+
+generateError('An error occured!', 500);
